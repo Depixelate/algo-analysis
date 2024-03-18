324347a5835a1bd2cd8b1d2b9fc8bdc853171bbd
Depixelate <91880097+Depixelate@users.noreply.github.com>
2024-03-18 11:59:28 +0530

    Did Ex3 and Ex4

---
+++ Ex3/1-inversions.py
@@ -0,0 +1,36 @@
+def merge_count(nums, start, end):
+	if start == end:
+		return 0
+	count = 0
+	mid = (start + end)//2
+	count += merge_count(nums, start, mid)
+	count += merge_count(nums, mid + 1, end)
+	count += merge(nums, start, end)
+	
+	return count
+
+def merge(nums, start, end):
+	tot_len = end - start + 1
+	aux = [0] * (tot_len)
+	count = 0
+	mid = (start + end) // 2
+	while i + j < tot_len:
+		if mid + 1 + j > end or 
+		(start + i < mid + 1 and nums[start + i] <= nums[mid + 1 + j]:
+			aux[i + j] = nums[start + i]
+			i += 1
+		else:
+			aux[i + j] = nums[mid + 1 + j]
+			count += mid + 1 - start - i
+			j += 1
+	for i in range(start, end + 1):
+		nums[i] = aux[i - start]
+	
+	return count
+		
+
+def count_inversions(nums):
+	return merge_count(nums, 0, len(nums) - 1)
+
+if __name__ == "__main__":
+	print(f"{count_inversions([3, 2, 8, 1])=}")
---
+++ Ex4/3-max_subarray_sum.py
@@ -0,0 +1,28 @@
+def max_subarray_sum(arr, start, end):
+	if start == end:
+		return arr[start]
+	mid = (start + end) // 2
+	max1 = max_subarray_sum(arr, start, mid)
+	max2 = max_subarray_sum(arr, mid + 1, end)
+	max_left = 0
+	cum_left = 0
+	for i in range(start, mid + 1):
+		cum_left += arr[i]
+		if cum_left > max_left:
+			max_left = cum_left
+	
+	max_right = 0
+	cum_right = 0
+	for i in range(mid + 1, end + 1):
+		cum_right += arr[i]
+		if cum_right > max_right:
+			max_right = cum_right
+			
+	max3 = max1 + max2
+	
+	return max(max1, max2, max3)
+	
+
+if __name__ == "__main__":
+	nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+	print("f{max_subarray_sum(nums, 0, len(nums)-1}")

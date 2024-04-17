e98ac395e651c22bb74f1d0208fb66107a425025
Sukessh Velusamy <sukesshvelusamy@gmail.com>
2024-04-17 16:28:35 +0530

    Modified.

--- Ex6/2-min_parenthesis.py
+++ Ex6/2-min_parenthesis.py
@@ -1,31 +1,66 @@
-from collections import deque
+import math
+# from collections import deque
 
-def is_valid(parenthesis:str):
+# def is_valid(parenthesis:str):
+#     count = 0
+#     for char in parenthesis:
+#         if char == '(':
+#             count += 1
+#         else:
+#             count -= 1
+#         if count < 0:
+#             return False
+
+#     return count == 0
+
+# def min_parens_remove(parenthesis):
+#     paren_queue = deque()
+#     paren_queue.append((parenthesis, 0))
+#     while len(paren_queue) > 0:
+#         possibility, num_parens_removed = paren_queue.popleft()
+#         if is_valid(possibility):
+#             print(f"Number of parenthesis removed = {num_parens_removed}")
+#             print(f"Output = {possibility}")
+#             return
+#         for i in range(len(possibility)):
+#             new_possibility = possibility[:i] + possibility[i+1:]
+#             if new_possibility not in paren_queue:
+#                 paren_queue.append((new_possibility, num_parens_removed+1))
+
+
+def is_valid(parens):
     count = 0
-    for char in parenthesis:
-        if char == '(':
+    for i in range(len(parens)):
+        if parens[i] == '(':
             count += 1
         else:
             count -= 1
+        
         if count < 0:
             return False
-        
+    
     return count == 0
 
-def min_parens_remove(parenthesis):
-    paren_queue = deque()
-    paren_queue.append((parenthesis, 0))
-    while len(paren_queue) > 0:
-        possibility, num_parens_removed = paren_queue.popleft()
-        if is_valid(possibility):
-            print(f"Number of parenthesis removed = {num_parens_removed}")
-            print(f"Output = {possibility}")
-            return
-        for i in range(len(possibility)):
-            new_possibility = possibility[:i] + possibility[i+1:]
-            if new_possibility not in paren_queue:
-                paren_queue.append((new_possibility, num_parens_removed+1))
+def min_parens(parens, num_rem, min_num_rem):
+    if num_rem >= min_num_rem:
+        return math.inf, None
+    
+    if is_valid(parens):
+        return num_rem, parens
 
+    min_rem_parens = None
+    for i in range(len(parens)):
+        paren = parens.pop(i)
+        possible_num_rem, possible_paren = min_parens(parens, num_rem + 1, min_num_rem)
+        if possible_num_rem < min_num_rem:
+            min_num_rem = possible_num_rem
+            min_rem_parens = list(possible_paren)
+        parens.insert(i, paren)
+    return min_num_rem, min_rem_parens
 
-parens = input("Enter the parenthesis: ")
-min_parens_remove(parens)
+if __name__ == "__main__":
+    parens = input("Enter the parenthesis: ")
+    num_rem, valid_parens = min_parens(list(parens), 0, math.inf)
+    print(f"Number of parenthesis removed: {num_rem}")
+    print(f"Valid Parenthesis: {''.join(valid_parens)}")
+
--- Ex6/3-graph_coloring.py
+++ Ex6/3-graph_coloring.py
@@ -1,28 +1,58 @@
-def color_graph(graph, num_colors, node_colors:dict[int,int]):
-    fully_colored = True
+# def color_graph(graph, num_colors, node_colors:dict[int,int]):
+#     fully_colored = True
 
-    for i in range(len(graph)):
-        if i not in node_colors:
-            fully_colored = False
-            remaining_colors = set(range(num_colors))
-            neighbor_colors = {node_colors[j] for j in range(len(graph)) if graph[i][j] == 1 and j in node_colors}
-            remaining_colors.difference_update(neighbor_colors)
+#     for i in range(len(graph)):
+#         if i not in node_colors:
+#             fully_colored = False
+#             remaining_colors = set(range(num_colors))
+#             neighbor_colors = {node_colors[j] for j in range(len(graph)) if graph[i][j] == 1 and j in node_colors}
+#             remaining_colors.difference_update(neighbor_colors)
 
-            for color in remaining_colors:
-                node_colors[i] = color
-                result = color_graph(graph, num_colors, node_colors)
+#             for color in remaining_colors:
+#                 node_colors[i] = color
+#                 result = color_graph(graph, num_colors, node_colors)
 
-                if result:
-                    return True
-                
-                node_colors.pop(i)
-    
-    return fully_colored        
+#                 if result:
+#                     return True
+
+#                 node_colors.pop(i)
+
+#     return fully_colored
+
+def color_graph(graph, num_colors, node_colors: dict[int,int], cur_node):
+    if len(node_colors) == len(graph):
+        return True
+
+    remaining_colors = set(range(num_colors))
+
+    for i, is_connected in enumerate(graph[cur_node]):
+        if i in node_colors and is_connected == 1:
+            remaining_colors.discard(node_colors[i])
+
+    for color in remaining_colors:
+        node_colors[cur_node] = color
+        if color_graph(graph, num_colors, node_colors, cur_node + 1):
+            return True
+        node_colors.pop(cur_node)
+
+    return False
 
 
-graph = [[0, 1, 1], [1, 0, 1], [1, 1, 0]]
+
+
+num_nodes = int(input("Enter the number of nodes in the graph: "))
+graph = []
+for i in range(num_nodes):
+    neighbors = [int(node) for node in input(f"Enter the nodes connected to node {i}: ").split(',')]
+    row = [0] * num_nodes
+    for neighbor in neighbors:
+        row[neighbor] = 1
+    graph.append(row)
+
+num_colors = int(input("Enter the number of colors: "))
+
 node_colors = {}
-result = color_graph(graph, 3, node_colors)
+result = color_graph(graph, num_colors, node_colors, 0)
 if result:
     print(f"Graph coloring: {node_colors}")
 else:
